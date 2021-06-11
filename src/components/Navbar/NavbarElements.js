1e23b1f36b9fcb0cf816733d8121377d0a4446ae
Amuleek Simak <amuleeksimak@gmail.com>
2021-06-11 18:37:51 -0400

    fixed resume button bug

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -95,7 +95,7 @@ export const NavBtn = styled.nav`
     }
 `;
 
-export const NavBtnLink = styled(LinkR)`
+export const NavBtnLink = styled.a`
     border-radius: 50px;
     background: #F9CAAD;
     white-space: nowrap;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {FaBars} from 'react-icons/fa'
+import Resume from "../../resources/Resume.pdf"
 import {
     Nav, 
     NavbarContainer, 
@@ -41,7 +42,7 @@ const Navbar = ({toggle}) => {
                             <NavLinks to='contact'> Contact </NavLinks>
                         </NavItem>
                     </NavMenu>
-                    <a href='https://drive.google.com/file/d/1zTsk6fwcBZj0OwkIjKPsuZg8B585iG9z/view?usp=sharing' style={{textDecoration: 'none', color: '#220504'}}>
+                    <a href={Resume} target="_blank" rel='noopener noreferrer' style={{textDecoration: 'none', color: '#220504'}}>
                         <NavBtn>
                             <NavBtnLink> Resume </NavBtnLink>
                         </NavBtn>
