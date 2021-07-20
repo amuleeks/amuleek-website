d88c8369429b65ec9a431b3069f81dd83b5547aa
Amuleek Simak <amuleeksimak@gmail.com>
2021-07-20 10:40:08 -0400

    updated experience section to have more precise job desc

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -16,23 +16,23 @@ const Experience = () => {
             <ExperienceH1>Experience</ExperienceH1>
                 <ExperienceWrapper>
                     <ExperienceCard>
-                        <ExperienceH2>Software Engineer Intern @ Investopedia</ExperienceH2>
+                        <ExperienceH2>Software Development Engineer Intern @ Investopedia</ExperienceH2>
                         <br></br>
                         <ExperienceP>
-                            <li>Developed new features for back-end of web application and fixed bugs found in the application</li>
+                            <li>Implemented script using <b>Java</b> and <b>Keycloak REST API</b> that adds confidential clients to Keycloak authentication to allow SSO integration between various websites </li>
                             <br></br>
-                            <li>Improved the performance of web applications utilized by <b>100 million</b> users per month </li>
+                            <li>Improved the performance of web applications utilized by 100 million users per month by <b>30%</b> by creating an efficient algorithm to process data </li>
                             <br></br>
-                            <li>Created endpoints for application and deployed them using <b>AWS</b></li>
+                            <li>Created endpoints for <b>15+</b> verticals and deployed them using <b>AWS Lambda</b></li>
                             <br></br>
-                            <li>Designed automation and unit tests using <b>JUnit</b> to tackle edge cases </li>
+                            <li> Developed custom exceptions to help identify and eliminate bugs found in code and to increase overall readability  </li>
                         </ExperienceP>
                     </ExperienceCard>
                     <ExperienceCard>
-                        <ExperienceH2>Programming Instructor @ Hatch</ExperienceH2>
+                        <ExperienceH2>JavaScript Coach @ Hatch</ExperienceH2>
                         <br></br>
                         <ExperienceP>
-                            <li>Worked with a team to instruct <b>JavaScript</b> to a large group of students</li>
+                            <li>Worked with a team to instruct <b>JavaScript</b> to a group of <b>40+</b> students</li>
                             <br></br>
                             <li>Corrected common syntax errors and collaborated with others to work through bugs</li>
                             <br></br>
@@ -40,12 +40,12 @@ const Experience = () => {
                         </ExperienceP>
                     </ExperienceCard>
                     <ExperienceCard>
-                        <ExperienceH2>Robotics Engineering Instructor @ FIRST Robotics</ExperienceH2>
+                        <ExperienceH2>Robotics Team Lead @ FIRST Robotics</ExperienceH2>
                         <br></br>
                         <ExperienceP>
                            <li>Supported the programming task force at TL Kennedy with debugging code written in <b>Python</b></li>
                            <br></br>
-                           <li>Identified hardware issues in the robot and developed solutions</li>
+                           <li>Improved precise movement coordination of robot's arm navigation by 70% by determining the joint angles</li>
                            <br></br>
                            <li>Competed at the ONT District Event, achieved the <b>17th rank</b> in the entire district</li>
                         </ExperienceP>
