9b655e714217f732c32e92fc7e2b23a699143883
Amuleek Simak <61286270+amuleeks@users.noreply.github.com>
2022-03-27 18:43:46 -0400

    Update Data.js

--- src/components/infoSection/Data.js
+++ src/components/infoSection/Data.js
@@ -5,7 +5,7 @@ export const homeObjOne = {
     lightTextDesc: true,
     topLine: 'About',
     headline: 'Who I am.',
-    description: 'I am a passionate computer science student always open to develop and learn new things. I love working in teams and meeting new people. Everyday, I aspire to grow and become better than the day before.',
+    description: 'I have always been a curious individual, Ive always loved asking myself how things work and going on quests to figure them out. This curiosity eventually led me towards programming which allowed me to help make peoples lives easier with some lines of code. This has guided me to my chosen career, Software Engineer positions that challenge me and allow me to use my creativity to come up with innovative solutions that make a large impact in the tech world. I spend a lot of my free time coding out the imaginative ideas I have at hackathons. Some of the hackathons I have attended include ElleHacks, DefHacks, and Hack the North. At Hack the North I made a project called Mntr that is a web application where students can connect to people in the career path they want to go into. I was able to collaborate with my team and learn a completely new language in the 48 hours we were given to complete the project. I was able to use new tools and adapt quickly to the situation and work with my team to make an appealing product. We ended up winning the Best Use of Cockroach DB which was such a phenomenal experience.',
     buttonLabel: 'Experience',
     //property below is how you change the side of the image
     imgStart: false,
